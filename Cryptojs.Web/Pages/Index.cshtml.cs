df1ac276e2076bc418cd030efc8b89ad70324480
İbrahim Ekinci <ibrahim.ekinci@next4biz.com>
2021-12-28 10:54:13 +0300

    v1
    v1

---
+++ Cryptojs.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using Cryptojs.Web.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cryptojs.Web.Pages
+{
+    public class IndexRequestModel
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string UserNameEncrypted { get; set; }
+        public string Password { get; set; } = string.Empty;
+        public string PasswordEncrypted { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Note { get; set; } = string.Empty;
+
+        [DataType(DataType.MultilineText)]
+        public string NoteEncrypted { get; set; }
+    }
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [FromForm]
+        public IndexRequestModel Model { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["result"] = "- not started";
+        }
+        public void OnPost()
+         {
+            var usernameDecrypted = AESEncryption.DecryptStringAES(Model.UserNameEncrypted);
+            bool usernameIsTrue = Model.UserName == usernameDecrypted;
+
+            var passwordDecrypted = AESEncryption.DecryptStringAES(Model.PasswordEncrypted);
+            bool passwordIsTrue = Model.Password == passwordDecrypted;
+
+            var noteDecrypted = AESEncryption.DecryptStringAES(Model.NoteEncrypted);
+            bool noteIsTrue = Model.Note == noteDecrypted;
+
+            var result = usernameIsTrue && passwordIsTrue && noteIsTrue;
+
+            ViewData["result"] = $"- result : {result}";
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,8 @@
+source
+	https://github.com/smartinmedia/Net-Core-JS-Encryption-Decryption
+
+dependencies
+	jquery
+	crypto-js(4.1.1)
+		https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
+		https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/aes.min.js
